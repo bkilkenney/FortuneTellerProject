412af8039f2a8a33200cbbc7a04be404c53f30fa
Brad Kilkenney <bkilkenney@gmail.com>
2016-09-09 15:19:41 -0400

    Initial Commit

---
+++ FortuneTellerProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FortuneTellerProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name?");
+            Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            Console.ReadLine();
+            Console.WriteLine("What is your birth month as a number?");
+            Console.ReadLine(); 
+
+            int birthMonth = 1;
+                if (birthMonth >= 1 || birthMonth <= 12);
+                Console.ReadLine();
+        }
+    }
+}
