7117e9a2adefb9babe561a8aa1582f28fa2258bd
Brad Kilkenney <bkilkenney@gmail.com>
2016-09-11 23:20:33 -0400

    Help still not working right

--- FortuneTellerProject/Program.cs
+++ FortuneTellerProject/Program.cs
@@ -13,8 +13,8 @@ namespace FortuneTellerProject
             Console.ForegroundColor = ConsoleColor.Red;
 
             Console.WriteLine("\aWhat is your first name?");
-            string fName = Console.ReadLine();            
-            
+            string fName = Console.ReadLine();
+
             Console.WriteLine("What is your last name?");
             string lname = Console.ReadLine();
 
@@ -24,29 +24,34 @@ namespace FortuneTellerProject
             Console.WriteLine("What is your birth month as a number?");
             int bMonth = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("What letter in ROYGBIV best represents your favorite color?");
-            char favColor = char.Parse(Console.ReadLine());
-
+            Console.WriteLine("What is your favorite ROYGBIV color?  If you are not familiar with ROYGBIV enter\"Help\"");
+            string favColor = Console.ReadLine();
+            string convertAllToLower = favColor.ToLower();
+            if (favColor == "Help")
+            {
+                Console.WriteLine("R is for Red\nO is for Orange\nY is for Yellow\nG is for Green\nB is for Blue\nI is for Indigo\nV is for Violet");
+            }
+            
             Console.WriteLine("How many siblings do you have?");
             int numOfSibs = int.Parse(Console.ReadLine());
 
-            string fullName = fName + " " + lname; 
+            string fullName = fName + " " + lname;
 
 //1st if
 
             if (age % 2 == 0)
             {
-                Console.WriteLine(fullName + ", you will retire in 63 years"); 
+                Console.WriteLine(fullName + ", you will retire in 63 years");
             }
 
-                else if(age % 2 != 0)
-                {
+            else if (age % 2 != 0)
+            {
                 Console.WriteLine(fullName + ", you will retire in 3 years");
-                }
-                    else
-                    {
-                    Console.WriteLine("Invalid Answer");
-                    }
+            }
+            else
+            {
+                Console.WriteLine("Invalid Answer");
+            }
 
 //2nd if
 
@@ -54,22 +59,22 @@ namespace FortuneTellerProject
             {
                 Console.WriteLine("with $200,000 in the bank");
             }
-                else if (bMonth >= 5 && bMonth <= 8)
-                {
-                    Console.WriteLine("with $6M in the bank");
-                }
-                else if (bMonth >= 9 && bMonth <= 12)
-                {
-                    Console.WriteLine("with $35.78 in the bank");
-                }
-                else if (bMonth < 1 || bMonth > 12)
-                {
-                    Console.WriteLine(", and there won't be any money, but when you die, on your deathbed, you will receive total consciousness.  Now go");
-                }
-                    else
-                    {
-                        Console.WriteLine("Invalid Answer");
-                    }
+            else if (bMonth >= 5 && bMonth <= 8)
+            {
+                Console.WriteLine("with $6M in the bank");
+            }
+            else if (bMonth >= 9 && bMonth <= 12)
+            {
+                Console.WriteLine("with $35.78 in the bank");
+            }
+            else if (bMonth < 1 || bMonth > 12)
+            {
+                Console.WriteLine(", and there won't be any money, but when you die, on your deathbed, you will receive total consciousness.  Now go");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Answer");
+            }
 
 //3rd if
 
@@ -77,62 +82,58 @@ namespace FortuneTellerProject
             {
                 Console.WriteLine("to your vacation home in an igloo");
             }
-                else if (numOfSibs == 1)
-                {
-                    Console.WriteLine("to your vacation home in Schenectady");
-                }
-                else if (numOfSibs == 2)
-                {
-                    Console.WriteLine("to your vacation home in Cleveland Hopkins International Airport Terminal C");
-                }
-                else if (numOfSibs == 3)
-                {
-                    Console.WriteLine("to your vacation home in Walla Walla Washington");
-                }
-                else if (numOfSibs <= 0 || numOfSibs >= 4)
-                {
-                    Console.WriteLine("to your vacation home at the BMV");
-                }
-                    else
-                    {
-                    Console.WriteLine(", you were going to have a vacation home but since you couldn't enter a valid number of sibling your future is unknown");
-                    }
+            else if (numOfSibs == 1)
+            {
+                Console.WriteLine("to your vacation home in Schenectady");
+            }
+            else if (numOfSibs == 2)
+            {
+                Console.WriteLine("to your vacation home in Cleveland Hopkins International Airport Terminal C");
+            }
+            else if (numOfSibs == 3)
+            {
+                Console.WriteLine("to your vacation home in Walla Walla Washington");
+            }
+            else if (numOfSibs <= 0 || numOfSibs >= 4)
+            {
+                Console.WriteLine("to your vacation home at the BMV");
+            }
+            else
+            {
+                Console.WriteLine(", you were going to have a vacation home but since you couldn't enter a valid number of sibling your future is unknown");
+            }
 
-//Last of the ifs
+//Last of the ifs. It is taking the char answer and parsing it to a int behind the scenes so ==, > operators will work.  But what about the help option?  
 
-            if (favColor == 'R' || favColor == 'r')
+            
+            if (favColor == "Red")
             {
                 Console.WriteLine("flying a dirigible.");                
             }
-            else if (favColor == 'O' || favColor == 'o')
+            else if(favColor == "Orange")
             {
                 Console.WriteLine("captaining the black pearl.");
             }
-            else if (favColor == 'Y' || favColor == 'y')
+            else if (favColor == "Yellow")
             {
                 Console.WriteLine("cruisin' on a vespa.");
             }
-            else if (favColor == 'G' || favColor == 'g')
+            else if (favColor == "Green")
             {
                 Console.WriteLine("inside a trojan horse.");
             }
-            else if (favColor == 'B' || favColor == 'b')
+            else if (favColor == "Blue")
             {
                 Console.WriteLine("driving the batmobile, the crappy original one from the 70s,\nnot the Christian Bale Dark Knight batmobile.");
             }
-            else if (favColor == 'I' || favColor == 'i')
+            else if (favColor == "Indigo")
             {
                 Console.WriteLine("riding a goat.");
             }
-            else if (favColor == 'V' || favColor == 'v')
+            else if (favColor == "Violet")
             {
                 Console.WriteLine("driving a power wheel.");
             }
-                else
-                {
-                Console.WriteLine("That wasn't a valid answer, but we'll move on.");
-                }
-
 
         }
     }
